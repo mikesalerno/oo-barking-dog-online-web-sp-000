7d2f7b0ac257439edc863cef072cd5f6d4f61832
Michael <wtfdic@hotmail.com>
2021-01-12 19:55:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,17 +2,17 @@
 #   def name=(dogs_name)
 #     @this_dogs_name = dogs_name
 #   end
-# 
+#
 #   def name
 #     @this_dogs_name
 #   end
-# 
+#
 #   def bark
 #     puts "woof!"
 #   end
 # end
 
-class Dog 
+class Dog
   def bark
     puts "Woof!"
 end
